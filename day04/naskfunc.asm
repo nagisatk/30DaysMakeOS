2c9a132e06209f25d31ea4a76bde2ba24e0e92dc
Nagisa <nagisa_tk@outlook.com>
2017-03-15 20:43:21 +0800

    4th day complete

--- day04/naskfunc.asm
+++ day04/naskfunc.asm
@@ -6,7 +6,11 @@
 [BITS 32]						; 制作32位模模式用的机械语言
 [FILE "naskfunc.asm"]			; 源文件名信息
 
-		GLOBAL	_io_hlt, _write_mem8			; 程序中包含的函数名
+        ; GLOBAL	_io_hlt, _write_mem8			; 程序中包含的函数名
+        GLOBAL  _io_hlt,    _io_cli,    _io_sti,    _io_stihlt
+        GLOBAL  _io_in8,    _io_in16,   _io_in32
+        GLOBAL  _io_out8,   _io_out16,  _io_out32
+        GLOBAL  _io_load_eflags,        _io_store_eflags
 
 
 ; 以下是实际的函数
@@ -16,7 +20,61 @@
 _io_hlt:	; void io_hlt(void);
 		HLT
 		RET
+_io_cli:    ; void io_vli(void);
+    CLI
+    RET
+_io_sti:    ; void io_sti(void);
+    STI
+    RET
+_io_stihlt: ; void io_stihlt(void);
+    STI
+    HLT
+    RET
+_io_in8:    ; int io_in8(int port);
+    MOV     EDX, [ESP + 4]      ; port
+    MOV     EAX, 0
+    IN      AL, DX
+    RET
+_io_in16:   ; int io_in16(int port);
+    MOV     EDX, [ESP + 4]      ; port
+    MOV     EAX, 0
+    IN      AX, DX
+    RET
+_io_in32:   ; int io_in32(int port);
+    MOV     EDX, [ESP + 4]      ; port
+    MOV     EAX, 0
+    IN      EAX, DX
+    RET
 
+_io_out8:    ; void _io_out8(int port, int data);
+    MOV     EDX, [ESP + 4]      ; port
+    MOV     AL, [ESP + 8]       ; data
+    OUT     DX, AL
+    RET
+_io_out16:   ; void _io_out16(int port, int data);
+    MOV     EDX, [ESP + 4]      ; port
+    MOV     AX, [ESP + 8]       ; data
+    OUT     DX, AX
+    RET
+_io_out32:   ; void _io_out32(int port, int data);
+    MOV     EDX, [ESP + 4]      ; port
+    MOV     EAX, [ESP + 8]       ; data
+    OUT     DX, EAX
+    RET
+_io_load_eflags:    ; int io_load_eflags(void);
+    PUSHFD          ; 指 PUSH EFLAGS
+    POP     EAX
+    RET
+_io_store_eflags:   ; void io_store_eflags(int eflags);
+    MOV     EAX, [ESP + 4]
+    PUSH    EAX
+    POPFD           ; 指 POP EFLAGS
+    RET
+
+
+
+    
+        
 _write_mem8:    ; void write_mem8(int addr, int data);
         MOV     ECX, [ESP + 4]  ; [ESP + 4]中存放的是地址， 将其读入ECX
         MOV     AL,  [ESP + 8]  ; [ESP + 8]中存放的是数据，将其读入AL
